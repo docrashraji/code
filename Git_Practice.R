f1346f50503a004768ce1095d89dc48a5083585f
docrashraji <rajirasheed@gmail.com>
2025-01-14 21:43:48 +0100

    life expectancy greater than 75

--- Git_Practice.R
+++ Git_Practice.R
@@ -4,7 +4,7 @@ library(dslabs)
 
 Pract_data <- dslabs::gapminder #
 
-results_gdp <- Pract_data |> filter(life_expectancy > 70) |> group_by(continent) |> summarise(mean_gdp=mean(gdp, na.rm = TRUE))
+results_gdp <- Pract_data |> filter(life_expectancy > 75) |> group_by(continent) |> summarise(mean_gdp=mean(gdp, na.rm = TRUE))
 
 write_csv(results_gdp, file = "gdp_continent.csv")
 
