f1fee5aa371f09311071732045bc463df0a93bb6
docrashraji <rajirasheed@gmail.com>
2025-01-14 22:09:59 +0100

    Some Words added

--- Git_Practice.R
+++ Git_Practice.R
@@ -2,12 +2,12 @@ library(tidyverse)
 
 library(dslabs)
 
-Pract_data <- dslabs::gapminder #
+Pract_data <- dslabs::gapminder 
 
-results_gdp <- Pract_data |> filter(life_expectancy > 75) |> group_by(continent) |> summarise(mean_gdp=mean(gdp, na.rm = TRUE))
+results_gdp <- Pract_data |> filter(life_expectancy > 75) |> group_by(continent) |> summarise(mean_gdp=mean(gdp, na.rm = T))
 
 
-write_csv(data_gdp, file = "gdp_continent.csv")
+write_csv(data_gdp, file = "gdp_continent.csv") #Use this code when saving a csv file
 
 gdp_graph <- Pract_data |> ggplot() +aes(year, gdp) + geom_point()
 ggsave(gdp_graph,file="gdp_graph.jpeg")
