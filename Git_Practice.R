84811af7f31d8e3539ee02ed69548e7f4b07a54c
docrashraji <rajirasheed@gmail.com>
2025-01-14 22:06:18 +0100

    Graph added

--- Git_Practice.R
+++ Git_Practice.R
@@ -10,5 +10,5 @@ results_gdp <- Pract_data |> filter(life_expectancy > 75) |> group_by(continent)
 write_csv(data_gdp, file = "gdp_continent.csv")
 
 gdp_graph <- Pract_data |> ggplot() +aes(year, gdp) + geom_point()
-ggsave(gdp_grapgh)
+ggsave(gdp_graph,file="gdp_graph.jpeg")
 
